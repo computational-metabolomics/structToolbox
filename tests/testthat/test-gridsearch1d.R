2401341c382fa4effae02d0fab7086679bf23e8b
grlloyd <g.r.lloyd@bham.ac.uk>
2019-04-05 16:29:11 +0100

    add more tests for grid search

--- tests/testthat/test-gridsearch1d.R
+++ tests/testthat/test-gridsearch1d.R
@@ -1,5 +1,5 @@
 # test grid search
-test_that('grid_search',{
+test_that('grid_search iterator',{
   set.seed('57475')
   # dataset
   D=iris_dataset()
@@ -20,7 +20,27 @@ test_that('grid_search',{
 })
 
 # test grid search
-test_that('grid_search',{
+test_that('grid_search wf',{
+  set.seed('57475')
+  # dataset
+  D=iris_dataset()
+  # iterator
+  I = grid_search_1d(param_to_optimise='number_components',
+    factor_name='Species',
+    search_values=1:4,
+    model_index=2,
+    max_min='min')*
+    (mean_centre()+PLSDA())
+  # metric
+  B=balanced_accuracy()
+  # run
+  I=run(I,D,B)
+  # calculate metric
+  expect_equal(I$metric$value[1],0.320)
+})
+
+# test grid search
+test_that('grid_search chart',{
   set.seed('57475')
   # dataset
   D=iris_dataset()
