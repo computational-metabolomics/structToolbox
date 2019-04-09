1375471e3a91cd6d6828bb473260b62568b6cae7
grlloyd <g.r.lloyd@bham.ac.uk>
2019-04-09 15:34:57 +0100

    add tests for rsd filter

---
+++ tests/testthat/test-rsd-filter.R
@@ -0,0 +1,25 @@
+# rsd filter
+test_that('rsd filter',{
+  set.seed('57475')
+  # dataset
+  D=iris_dataset()
+  # method
+  M = rsd_filter(qc_label='virginica',factor_name='Species',rsd_threshold=100)
+  # apply
+  M=method.apply(M,D)
+  expect_true(all(M$flags$rsd_flags==1))
+})
+
+test_that('blank filter histogram',{
+  set.seed('57475')
+  # dataset
+  D=iris_dataset()
+  # method
+  M = rsd_filter(qc_label='virginica',factor_name='Species',rsd_threshold=100)
+  # apply
+  M=method.apply(M,D)
+  # chart
+  C = rsd_filter.hist()
+  gg=chart.plot(C,M)
+  expect_true(is(gg,'ggplot'))
+})
