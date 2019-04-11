d8ba5eca9a0c83e48942cb089d44b2da40e382fd
grlloyd <g.r.lloyd@bham.ac.uk>
2019-04-11 10:09:09 +0100

    fix broken knn-imput test
    instead of random NA throughout matrix, using a single NA at known row/col location

--- tests/testthat/test-knn-impute.R
+++ tests/testthat/test-knn-impute.R
@@ -3,16 +3,10 @@ test_that('knn impute',{
   set.seed('57475')
   # dataset
   D=iris_dataset()
-  D$data=as.data.frame(apply(D$data,2,function(x){
-    n=seq(1,150,by=4)
-    s=sample(1:length(n),length(n),replace=TRUE)
-    n=n[-s]
-    x[n]=NA
-    return(x)
-  }))
+  D$data[5,1]=NA
   # method
   M = knn_impute(neighbours=5)
   # apply
   M = method.apply(M,D)
-  expect_equal(M$imputed$data[5,1],4.675,tolerance=0.001) # an imputed value
+  expect_equal(M$imputed$data[5,1],5.2,tolerance=0.001) # an imputed value
 })
