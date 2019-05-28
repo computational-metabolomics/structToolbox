4192618e8f16a2a33987ecd8d23f6491b4e33766
grlloyd <g.r.lloyd@bham.ac.uk>
2019-05-28 14:46:57 +0100

    Update factor_barchart_class.R

--- R/factor_barchart_class.R
+++ R/factor_barchart_class.R
@@ -37,6 +37,8 @@ setMethod(f="chart.plot",
     definition=function(obj,dobj)
     {
 
+        X=dobj$data
+
         if (is.numeric(obj$feature_to_plot)) {
             varn=colnames(X)[obj$feature_to_plot]
         } else {
