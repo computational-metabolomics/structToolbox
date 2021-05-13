9951faedc36bcdd1678f852db26ef0e95734bfb1
Gavin Rhys Lloyd <g.r.lloyd@bham.ac.uk>
2021-05-13 13:43:46 +0100

    replace vip_summary with feature_importance
    renamed and now allows vip, sr and sr_pvalues to be plotted

--- R/plsda_feature_significance_chart.R
+++ R/plsda_feature_significance_chart.R
@@ -1,32 +1,34 @@
-#' @eval get_description('plsda_vip_summary_plot')
-#' @export plsda_vip_summary_plot
+#' @eval get_description('plsda_feature_importance_plot')
+#' @export plsda_feature_importance_plot
 #' @include PLSDA_class.R
 #' @examples
 #' D = iris_DatasetExperiment()
 #' M = mean_centre()+PLSDA(factor_name='Species')
 #' M = model_apply(M,D)
 #'
-#' C = plsda_vip_summary_plot(n_features=30)
+#' C = plsda_feature_importance_plot(n_features=30,metric='vip')
 #' chart_plot(C,M[2])
-plsda_vip_summary_plot = function(n_features=50,...) {
-    out=struct::new_struct('plsda_vip_summary_plot',
+plsda_feature_importance_plot = function(n_features=30,metric='vip',...) {
+    out=struct::new_struct('plsda_feature_importance_plot',
         n_features=n_features,
+        metric=metric,
         ...)
     return(out)
 }
 
-.plsda_vip_summary_plot<-setClass(
-    "plsda_vip_summary_plot",
+.plsda_feature_importance_plot<-setClass(
+    "plsda_feature_importance_plot",
     contains=c('chart'),
     slots=c(
-        n_features='entity'
+        n_features='entity',
+        metric='enum'
     ),
     prototype = list(
-        name='PLSDA VIP summary plot',
-        description='A plot of the Variable Importance for Projection (VIP) scores for a PLSDA model for the top selected features.',
+        name='PLSDA feature importance summary plot',
+        description='A plot of the selected feature significance metric for a PLSDA model for the top selected features.',
         type="chart",
         libraries=c('pls','ggplot2','reshape2','cowplot'),
-        .params=c('n_features'),
+        .params=c('n_features','metric'),
         
         n_features = entity(
             name = 'Number of features',
@@ -34,19 +36,39 @@ plsda_vip_summary_plot = function(n_features=50,...) {
             type=c('numeric','integer'),
             value=50,
             max_length=1
+        ),
+        metric=enum(
+            name='Metric to plot',
+            description=c(
+                'sr' = 'Plot Selectivity Ratio',
+                'pvalue'='Plot SR p-values',
+                'vip'='Plot Variable Importance in Projection scores'
+            ),
+            type='character',
+            allowed=c('sr','sr_pvalue','vip'),
+            value='vip'
         )
     )
-    
 )
+
 
 #' @export
 #' @template chart_plot
 setMethod(f="chart_plot",
-    signature=c("plsda_vip_summary_plot",'PLSDA'),
+    signature=c("plsda_feature_importance_plot",'PLSDA'),
     definition=function(obj,dobj) {
         
         # max vip
-        data=dobj$vip
+        if (obj$metric=='sr') {
+            data=dobj$sr
+            txt='Selectivity Ratio'
+        } else if(obj$metric=='sr_pvalue') {
+            data=dobj$sr_pvalue
+            txt='SR p-value'
+        } else if(obj$metric=='vip') {
+            data=dobj$vip
+            txt='VIP score'
+        }
         
         # max sure we dont over the max number of features
         obj$n_features=min(nrow(data),obj$n_features)
@@ -55,8 +77,14 @@ setMethod(f="chart_plot",
         data$feature_id=rownames(data)
         data$max=max_vip[,1]
         
-        # sort by max vip
-        vip_order=order(-data$max)
+        # sort by max SR, or min pvalue
+        if (obj$metric=='sr'){
+            vip_order=order(-data$max)
+        } else if(obj$metric=='sr_pvalue') {
+            vip_order=order(data$max)
+        } else if(obj$metric=='vip') {
+            vip_order=order(-data$max)
+        }
         data=data[vip_order,]
         
         data2=reshape2::melt(data[1:obj$n_features,seq_len(ncol(data)-1)],id.vars = 'feature_id')
@@ -64,7 +92,7 @@ setMethod(f="chart_plot",
         
         g1 = ggplot(data=data[1:obj$n_features,],aes_string(x='max',y='reorder(feature_id,max)')) +
             geom_point() +
-            labs(x="VIP score",
+            labs(x=txt,
                 y="Feature") +
             theme_Publication() +
             theme(
@@ -77,7 +105,7 @@ setMethod(f="chart_plot",
             geom_tile(aes_string(fill='value'),colour = "black",width=0.8,height=0.8) +
             scale_fill_gradient2(low='#5e4fa2',mid='#ffffbf',high='#9e0142',
                 midpoint=(quantile(data2$value,0.05)+quantile(data2$value,0.95))/2,
-                limits=c(quantile(data2$value,0.05),quantile(data2$value,0.95)),oob=scales::squish,name='VIP score') +
+                limits=c(quantile(data2$value,0.05),quantile(data2$value,0.95)),oob=scales::squish,name=txt) +
             theme_Publication() +
             theme(axis.title.y=element_blank(),
                 axis.text.y=element_blank()) +
